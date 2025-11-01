cc77ca37befe510a15edf4a48788bbcc34f72b4d
Pragmatic <cryptopragmatic@protonmail.com>
2025-11-01 16:01:53 +0100

    disabled forward

--- monitor.go
+++ monitor.go
@@ -267,7 +267,7 @@ func initMonitor() *Monitor {
 	go m.monitorAintBuys()
 	go m.monitorNodeMints()
 	go m.monitorDiskSpace()
-	go m.forwardCompetition()
+	// go m.forwardCompetition()
 	go m.monitorNodes()
 	return m
 }
